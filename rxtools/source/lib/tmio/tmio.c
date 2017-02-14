2f2150efcab56cd791e0fabe201e7802c5b1bbfe
dukesrg <dukesrg@gmail.com>
2017-02-14 16:52:50 +0300

    Once again

--- rxtools/source/lib/tmio/tmio.c
+++ rxtools/source/lib/tmio/tmio.c
@@ -225,7 +225,7 @@ uint32_t tmio_writesectors(enum tmio_dev_id target, uint32_t sector_no, uint_fas
 //		tmio_send_command(MMC_SELECT_CARD | TMIO_CMD_RESP_R1, dev->RCA, 0);
 		char tmpstr[32];
 		sprintf(tmpstr, "%08lX %08lX", REG_MMC_IRQ_STATUS, REG_MMC_ERROR_DETAIL);
-		DrawStringRect(&top1Screen, lang(tmpstr), &(Rect){0, 0, 400, 32}, RED, style.valueAlign, 16);
+		DrawStringRect(&top1Screen, lang(tmpstr), &(Rect){0, 0, 400, 32}, RED, ALIGN_LEFT, 16);
 		DisplayScreen(&top1Screen);
 		while(1);
 	}
