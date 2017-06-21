65e2a7ef4d9ffe45386792834e65bdd6d1daf02a
dukesrg <dukesrg@gmail.com>
2017-06-21 23:30:35 +0300

    ticket.db format parser for key extract (#62)

--- rxtools/source/lib/ticket.c
+++ rxtools/source/lib/ticket.c
@@ -23,7 +23,7 @@
 #include "ticket.h"
 #include "native_firm.h"
 #include "nand.h"
-
+#include "blobs.h"
 #include "tmd.h"
 #include "firm.h"
 #include "mpcore.h"
@@ -117,7 +117,7 @@ uint_fast8_t ticketGetKey(aes_key *key, uint64_t titleid, uint_fast8_t drive) {
 	wchar_t path[_MAX_LFN + 1];
 	ticket_data *ticket;
 
-	swprintf(path, sizeof(path), L"%u:dbs/ticket.db", drive);
+	swprintf(path, _MAX_LFN + 1, L"%u:dbs/ticket.db", drive);
 
 	if (!FileOpen(&fil, path, 0))
 		return 0;
@@ -134,6 +134,94 @@ uint_fast8_t ticketGetKey(aes_key *key, uint64_t titleid, uint_fast8_t drive) {
 					FileClose(&fil);
 					return decryptKey(key, ticket);
 				}
+	FileClose(&fil);
+	return 0;
+}
+
+#define BDRI_SEEK_PENDING	0
+#define BDRI_SEEK_FAILED	0xFFFFFFFF
+
+uint_fast8_t ticketGetKey2(aes_key *key, uint64_t titleid, uint_fast8_t drive) {
+	static uint32_t table_offset = BDRI_SEEK_PENDING;
+	static uint32_t block_size;
+	static uint32_t entries_total;
+
+	File fil;
+	wchar_t path[_MAX_LFN + 1];
+	diff_header diff;
+	difi_header difi;
+	ivfc_v2_header ivfc;
+	dpfs_header dpfs;
+	tick_header tick;
+	bdri_header bdri;
+        size_t difi_offset, tick_offset;
+	uint8_t *header1;
+	title_entry_header_2 header2;
+
+	swprintf(path, _MAX_LFN + 1, L"%u:dbs/ticket.db", drive);
+	if (!FileOpen(&fil, path, 0))
+		return 0;
+	
+	switch(table_offset) {
+		case BDRI_SEEK_PENDING:
+			if (FileSeek(&fil, sizeof(aes_cmac_header)) &&
+				FileRead2(&fil, &diff, sizeof(diff)) == sizeof(diff) &&
+				diff.magic == (uint32_t)DIFF_MAGIC &&
+				(difi_offset = diff.active_partition == DIFF_PARTITION_PRIMARY ? diff.primary_partition_offset : diff.secondary_partition_offset) &&
+				FileSeek(&fil, difi_offset) &&
+				FileRead2(&fil, &difi, sizeof(difi)) == sizeof(difi) &&
+				difi.magic == (uint32_t)DIFI_MAGIC &&
+				FileSeek(&fil, difi_offset + difi.ivfc_offset) &&
+				FileRead2(&fil, &ivfc, sizeof(ivfc)) == sizeof(ivfc) &&
+				ivfc.magic == (uint32_t)IVFC_MAGIC &&
+				ivfc.format_version == IVFC_VERSION_2 &&
+				FileSeek(&fil, difi_offset + difi.dpfs_offset) &&
+				FileRead2(&fil, &dpfs, sizeof(dpfs)) == sizeof(dpfs) &&
+				dpfs.magic == (uint32_t)DPFS_MAGIC &&
+				(tick_offset = diff.file_base_offset + dpfs.ivfc_partiton_offset + (diff.active_partition == DIFF_PARTITION_PRIMARY ? 0 : dpfs.ivfc_partiton_size) + ivfc.l4_offset_lo) &&
+				FileSeek(&fil, tick_offset) &&
+				FileRead2(&fil, &tick, sizeof(tick)) == sizeof(tick) &&
+				tick.magic == (uint32_t)TICK_MAGIC &&
+				FileRead2(&fil, &bdri, sizeof(bdri)) == sizeof(bdri) &&
+				bdri.magic == (uint32_t)BDRI_MAGIC &&
+				(block_size = bdri.block_size) &&
+				(table_offset = tick_offset + sizeof(tick) + bdri.title_entry_table_offset) &&
+				FileSeek(&fil, table_offset) &&
+				(header1 = __builtin_alloca(block_size)) &&
+				FileRead2(&fil, header1, block_size) == block_size &&
+				FileRead2(&fil, &header2, sizeof(header2)) == sizeof(header2) &&
+				(entries_total = header2.entries_total)
+			) break;
+			table_offset = BDRI_SEEK_FAILED;
+		case BDRI_SEEK_FAILED:
+			FileClose(&fil);
+			return 0;
+		default:
+			if (FileSeek(&fil, table_offset + block_size + sizeof(header2)))
+				break;
+			FileClose(&fil);
+			return 0;
+	}
+
+	ticket_title_entry entries[entries_total];
+	if (FileRead2(&fil, entries, sizeof(entries)) == sizeof(entries)) {
+		cetk_data data;
+		ticket_title_info info;
+		uint32_t entry_offset;
+		titleid = __builtin_bswap64(titleid); //ticket entry table have little-endian title ID
+		for (size_t i = 0; i < entries_total; i++)
+			if (entries[i].title_id == titleid && entries[i].active)
+				return (entry_offset = table_offset + entries[i].title_info_offset * block_size) &&
+					FileSeek(&fil, entry_offset) &&
+					FileRead2(&fil, &info, sizeof(info)) == sizeof(info) &&
+					entries[i].title_info_size == info.size + sizeof(info) && //several active entries contains trash data, so size check used to filter wrong
+					FileRead2(&fil, &data.sig_type, sizeof(data.sig_type)) == sizeof(data.sig_type) &&
+					FileSeek(&fil, entry_offset + sizeof(info) + signatureAdvance(data.sig_type)) &&
+					FileRead2(&fil, &data.ticket, sizeof(data.ticket)) == sizeof(data.ticket) &&
+					(FileClose(&fil) || 1) &&
+					decryptKey(key, &data.ticket);
+	}
+
 	FileClose(&fil);
 	return 0;
 }
